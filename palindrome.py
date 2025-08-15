8ba2bd025f01884fb5e4b1644d9f7ba483d5a4af
Sravyalgorythm <sravyathepillar@gmail.com>
2025-08-15 11:57:07 +0530

    Add files via upload
    This program takes a string input given by the user and detects whether it is a palindrome or not.

---
+++ palindrome.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 13 19:49:22 2025
+
+@author: Usha
+"""
+
+a= input('Enter a string: ')
+k= a[::-1]
+if k==a:
+    print('Palindrome')
+else:
+    print('Not palindrome')
+    
+    
